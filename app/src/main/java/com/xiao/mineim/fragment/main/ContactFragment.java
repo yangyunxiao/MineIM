55839cb10230503d2e77ed2b6d54691a6df65eed
xiao <97598032@qq.com>
2018-07-09 07:40:29 +0800

    sync

--- app/src/main/java/com/xiao/mineim/fragment/main/ContactFragment.java
+++ app/src/main/java/com/xiao/mineim/fragment/main/ContactFragment.java
@@ -3,21 +3,53 @@ package com.xiao.mineim.fragment.main;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.xiao.common.app.BaseFragment;
+import com.xiao.common.widget.EmptyView;
+import com.xiao.common.widget.recycler.RecyclerAdapter;
+import com.xiao.factory.model.db.User;
 import com.xiao.mineim.R;
+
+import butterknife.BindView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContactFragment extends BaseFragment {
 
+    @BindView(R.id.contact_empty)
+    EmptyView mEmpty;
+
+    @BindView(R.id.contact_recycler)
+    RecyclerView mContactRecycler;
+
+    private RecyclerAdapter<User> mUserAdapter;
+
     @Override
     protected int getContentLayoutID() {
 
         return R.layout.fragment_contact;
+    }
+
+    @Override
+    protected void initWidget(View rootView) {
+        super.initWidget(rootView);
+        mContactRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        mContactRecycler.setAdapter(mUserAdapter = new RecyclerAdapter<User>() {
+            @Override
+            public int getItemViewType(int position, User user) {
+                return 0;
+            }
+
+            @Override
+            protected ViewHolder<User> onCreateViewHolder(View root, int viewType) {
+                return null;
+            }
+        });
     }
 }
--- common/src/main/java/com/xiao/common/Common.java
+++ common/src/main/java/com/xiao/common/Common.java
@@ -8,7 +8,7 @@ public class Common {
         //手机号码的正则
         String REGEX_MOBILE = "[1][3,4,5,7,8][0-9]{9}$";
 
-        String HOST = "http://192.168.137.171:8080/";
+        String HOST = "http://192.168.3.64:8080/";
 
         String API_URL = HOST + "api/";
 
